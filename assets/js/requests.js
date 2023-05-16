7964fcfcf0b657cb07f3fdddd72a9dbd5ecea7a6
Aether <tifsnunes@gmail.com>
2023-05-16 16:23:16 -0300

    request get post put delete

--- assets/js/requests.js
+++ assets/js/requests.js
@@ -15,5 +15,86 @@ async function insert(){
     let json = await response.json()
     console.log(json)
 }
-
 document.querySelector('#insert').addEventListener('click', insert)
+
+//request get
+async function getPost(){
+    let response = await fetch('https://jsonplaceholder.typicode.com/posts/1',
+     {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+     })
+    let json = await response.json()
+    console.log(json)
+}
+
+
+// request get comments
+async function getPostComments(){
+    let response = await fetch('https://jsonplaceholder.typicode.com/posts/1/comments',
+     {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+     })
+    let json = await response.json()
+    console.log(json)
+}
+
+
+//request get comments id
+async function getPostCommentsId(){
+    let response = await fetch('https://jsonplaceholder.typicode.com/posts/1/comments?postId=1',
+     {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+     })
+    let json = await response.json()
+    console.log(json)
+}
+
+
+// request put posts
+async function putPosts(){
+    let response = await fetch('https://jsonplaceholder.typicode.com/posts/1',
+     {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json' // especificação das informações que estou mandando
+        },
+        body: JSON.stringify({
+            title: 'test',
+            body: 'Body test',
+            userId: 2
+        })
+     })
+    let json = await response.json()
+    console.log(json)
+}
+
+// detete
+async function deletePosts(){
+    let response = await fetch('https://jsonplaceholder.typicode.com/posts/1',
+     {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json' // especificação das informações que estou mandando
+        }
+     })
+    let json = await response.json()
+    console.log(json)
+}
+
+// chamar os treco
+Promise.all([
+    getPost(),
+    getPostComments(),
+    getPostCommentsId(),
+    putPosts(),
+    deletePosts(),
+])
