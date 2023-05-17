653ae21b88e1bda56ef4d315b5c91ac51bd69c1b
Aether <tifsnunes@gmail.com>
2023-05-17 14:26:26 -0300

    requests att

--- assets/js/requests.js
+++ assets/js/requests.js
@@ -98,3 +98,19 @@ Promise.all([
     putPosts(),
     deletePosts(),
 ])
+
+//async await
+
+async function click(){
+    let response = await fetch('https://jsonplaceholder.typicode.com/posts')
+    let json = await response.json()
+    alert(`title of the 1st post: ${json[0].title}`)
+
+    alert('click!')
+}
+
+document.querySelector('#button3').addEventListener('click', click)
+
+// função anonima com async
+// let click = async () => {}
+
